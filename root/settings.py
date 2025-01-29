1aa0dc689d218c8d96ce3e05e425d3e4cca592ae
absaitovdevv <ficev50489@evusd.com>
2025-01-29 20:15:10 +0500

    add drf , swagger

--- root/settings.py
+++ root/settings.py
@@ -20,9 +20,11 @@ INSTALLED_APPS = [
     'django.contrib.admin',
 
     # My App
-    'authentication'
+    'authentication',
 
     # Third Part App
+    'rest_framework',
+    'drf_spectacular'
 
 
 ]
@@ -102,3 +104,21 @@ MEDIA_ROOT = join(BASE_DIR , 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# -------------------------------------------
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
