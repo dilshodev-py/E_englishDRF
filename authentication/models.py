fb418ed88562f8fba8954fbe644b6fa29e3e8ba9
absaitovdevv <ficev50489@evusd.com>
2025-01-31 20:32:00 +0500

    Make add celery

--- authentication/models.py
+++ authentication/models.py
@@ -41,19 +41,9 @@ class User(AbstractUser):
     full_name = CharField(max_length=128)
     email = EmailField(unique=True, null=False, blank=False)
     username = CharField(max_length=128, unique=False)
-
     objects = CustomUserManager()
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-    def create_superuser(self, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user( email, password, **extra_fields)
 
