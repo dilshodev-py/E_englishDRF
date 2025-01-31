8bfbf73025da9b68f35a292496366e6618130ea9
jezoww <ismoilov000d@gmail.com>
2025-01-31 19:58:34 +0500

    Fixed register!

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,25 +1,13 @@
 from django.urls import path
-
-
-from authentication.views import PasswordResetView
-
-from django.urls import path
-
-from django.urls import path
-
-from authentication.views import ForgotPasswordAPIView, ForgotPasswordCheckAPIView
-
-from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
+from authentication.views import ForgotPasswordAPIView, ForgotPasswordCheckAPIView, PasswordResetView, RegisterAPIView
 
 urlpatterns = [
     path('forgot-password', ForgotPasswordAPIView.as_view()),
     path('verify-otp', ForgotPasswordCheckAPIView.as_view()),
     path('auth/reset-password/', PasswordResetView.as_view()),
-
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterAPIView.as_view()),
 ]
-
--- authentication/views.py
+++ authentication/views.py
@@ -1,23 +1,20 @@
 import random
 from random import randint
 
+from authentication.models import User
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
 from django.http import JsonResponse
-from django.shortcuts import render
-from django.views.generic import CreateView
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.views import APIView
 
-from authentication.serializers import EmailSerializer
 from authentication.serializers import ForgotPasswordSerializer, ForgotPasswordCheckSerializer
-from authentication.tasks import send_email
+from authentication.serializers import PasswordResetSerializer
 from authentication.serializers import RegisterSerializer
 from authentication.tasks import send_email
-from authentication.serializers import PasswordResetSerializer
 
 
 @extend_schema(tags=['auth'], request=PasswordResetSerializer)
@@ -40,7 +37,7 @@ class ForgotPasswordAPIView(APIView):
             code = randint(10000, 99999)
             send_email.delay(to_send=email, code=code)
             response = JsonResponse({"message": "Code send to your email!", "email": email}, status=HTTP_200_OK)
-            response.set_cookie("code", str(code), max_age=300)
+            response.set_cookie("code", make_password(str(code)), max_age=300)
             return response
         return JsonResponse(s.errors, status=HTTP_400_BAD_REQUEST)
 
@@ -58,25 +55,23 @@ class ForgotPasswordCheckAPIView(APIView):
             return JsonResponse({"message": "Correct code!"}, status=HTTP_200_OK)
         return JsonResponse(s.errors, status=HTTP_400_BAD_REQUEST)
 
-
-
-
-# Create your views here.
-class RegisterAPIView(CreateView):
+@extend_schema(tags=['auth'], request=RegisterSerializer)
+class RegisterAPIView(CreateAPIView):
     def post(self, request, *args, **kwargs):
         serializer = RegisterSerializer(data=request.data)
         user = User.objects.filter(email=request.data.get("email")).first()
         if serializer.is_valid() or (user and not user.is_active):
             if not user:
-                serializer.save()
+                userr = serializer.save()
+                userr.is_active = False
+                userr.save()
             data = serializer.validated_data
             random_code = random.randrange(10 ** 5, 10 ** 6)
             email = data.get("email")
-            # send_email.delay(email, random_code)
+            send_email.delay(email, random_code)
             response = Response("Tasdiqlash kodi jo'natildi !", status=HTTP_200_OK)
             response.set_cookie("verify", make_password(str(random_code)))
             return response
         elif user and user.is_active:
             return Response("Email oldin ro'yxatdan o'tgan !", status=HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
