6b61de955b01a54bb5fc03b7110340a71a546afd
codem-py <ibehruz005@gmail.com>
2025-02-03 20:49:35 +0500

    Unit

--- essential/urls.py
+++ essential/urls.py
@@ -4,5 +4,5 @@ from essential.views import BookListAPIView, UniteListAPIView
 
 urlpatterns = [
     path('books/', BookListAPIView.as_view()),
-    path('units/', UniteListAPIView.as_view()),
+    path('books/<int:book_id>/units/', UniteListAPIView.as_view()),
 ]
