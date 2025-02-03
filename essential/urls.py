5f77436d9ecd55c66365a0601bf2566e0edf3207
absaitovdevv <ficev50489@evusd.com>
2025-02-03 20:42:22 +0500

    path

--- essential/urls.py
+++ essential/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from essential.views import QuizView
 
 urlpatterns = [
-    path('/random-quiz/',QuizView.as_view(), name='random-quiz')
+    path('random-quiz/',QuizView.as_view(), name='random-quiz')
 ]
